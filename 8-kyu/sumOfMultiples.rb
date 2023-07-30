f07a340cff766c50a84b71e60399aa2096d7c0ff
neminx <101298482+CarlosCastanedaDev@users.noreply.github.com>
2023-07-30 10:34:41 -0500

    solution 7/30/23

---
+++ 8-kyu/sumOfMultiples.rb
@@ -0,0 +1,23 @@
+=begin
+Find the sum of all multiples of n below m
+
+Keep in Mind
+n and m are natural numbers (positive integers)
+m is excluded from the multiples
+Examples
+sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+sumMul(4, -7)  ==> "INVALID"
+=end
+
+def sum_mul(n, m)
+  if  m <= 0 || n >= m
+    return "INVALID"
+  end
+  sum = 0
+  n.step(m-1,n) do |x|
+    sum += x
+  end 
+  sum
+end
