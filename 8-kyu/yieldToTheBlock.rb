605738b89855a2883426b2d11b61ef3153668d99
neminx <101298482+CarlosCastanedaDev@users.noreply.github.com>
2023-07-31 11:06:13 -0500

    solution 7/31/23

---
+++ 8-kyu/yieldToTheBlock.rb
@@ -0,0 +1,9 @@
+# Complete the Compute method/function so that if a block is given it will run else it returns "Do not compute".
+
+def compute
+  if block_given?
+    yield
+  else
+    "Do not compute"
+  end
+end
