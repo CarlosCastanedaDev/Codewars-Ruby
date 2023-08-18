04e3eb837d7bc6d762bb58e7477c3a49603e687c
CarlosCastanedaDev <carlos@nemin.dev>
2023-08-18 09:17:38 -0500

    solution 8/18/23

---
+++ 8-kyu/double_char.rb
@@ -0,0 +1,15 @@
+# Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+# Examples (Input -> Output):
+# * "String"      -> "SSttrriinngg"
+# * "Hello World" -> "HHeelllloo  WWoorrlldd"
+# * "1234!_ "     -> "11223344!!__  "
+# Good Luck!
+
+def double_char(str)
+  double = ''
+  str.chars.each do |char|
+    double += char + char
+  end
+  double
+end
