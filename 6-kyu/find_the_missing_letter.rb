d471e4699161efab84ca2adcd4315892dc2bc749
Carlos Castaneda <101298482+CarlosCastanedaDev@users.noreply.github.com>
2024-02-20 10:04:01 -0600

    solution 2/20/24

---
+++ 6-kyu/find_the_missing_letter.rb
@@ -0,0 +1,26 @@
+# Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+# You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+# The array will always contain letters in only one case.
+
+# Example:
+
+# ['a','b','c','d','f'] -> 'e'
+# ['O','Q','R','S'] -> 'P'
+# (Use the English alphabet with 26 letters!)
+
+# Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+# I have also created other katas. Take a look if you enjoyed this kata!
+
+def find_missing_letter(arr)
+  ascii = arr.join('').codepoints
+  start_point = ascii[0]
+  ascii.each_with_index do |e,i|
+    if e == start_point + i
+      next
+    else
+      return (e - 1).chr
+    end
+  end
+end
